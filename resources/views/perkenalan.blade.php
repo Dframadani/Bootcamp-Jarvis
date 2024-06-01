a5615b1b3261c1791e6cd5030bec3b59c89fdbef
unknown <dafaramadani845@gmail.com>
2024-06-01 13:43:33 +0700

    Pertemuan 6

---
+++ resources/views/perkenalan.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perkenalan</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="main-container">
+    <section id="hero">
+        <div class="hero-left">
+          <h3 class="pre-title">Hello</h3>
+          <h1 class="hero-name">Dafa <span>Ramadani</span></h1>
+          <p>
+            Hello, my name is Dafa Ramadani, I study at the Nurul Fikri Integrated STT campus, my hobbies are business and sports, I learned coding from scratch and I like to do anything as long as it's interesting
+          </p>
+        </div>
+        <div class="hero-right">
+          <img src="assets/foto dafa.jpeg" alt="foto dafa" />
+        </div>
+      </section>
+    </div>
+</body>
+</html>
+
